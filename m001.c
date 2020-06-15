1b9623636ce18c582acb714ea2eb08ed816c8134
GabrieldeSouza2 <gabrielsouza171216@usp.br>
2020-06-15 05:33:21 +0000

    m001

--- m001.c
+++ m001.c
@@ -28,22 +28,14 @@ int greatest (int a, int b, int c)
   {
     return a;
   }
-  else
-       {
-          if((b>a) && (b>c))
-          {
-            return b;
-          }
-          else
-              {
-                if((c>a) && (c>b))
-                {
-                   return c;
-                }
-
-              }
-         
-       }
+  if((b>a) && (b>c))
+  {
+    return b;
+  }
+   if((c>a) && (c>b))
+  {
+    return c;
+  }
  
 }
 
